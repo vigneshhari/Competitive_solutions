3a733a19149f3f3e63d289f8ec8e04cf1bcbbdf7
Vignesh Hari <vichuhari100@gmail.com>
2018-04-24 12:20:20 +0530

    It is never brute forcing .. **NEVER** .. Learned that the hard way

--- DAA/Python/BellManFord.py
+++ DAA/Python/BellManFord.py
@@ -57,8 +57,8 @@ class Graph:
  
 g = Graph(5)
 g.addEdge(0, 1, -1)
-g.addEdge(0, 2, 4)
-g.addEdge(1, 2, 3)
+g.addEdge(1, 2, 4)
+g.addEdge(2, 0, -3)
 g.addEdge(1, 3, 2)
 g.addEdge(1, 4, 2)
 g.addEdge(3, 2, 5)
