b6cb4dd5cee5550c474bb042c9fd24afa76cc4c0
Vignesh Hari <vichuhari100@gmail.com>
2018-05-01 00:43:26 +0530

    problem 18 and 67 in Project Euler Done

--- Project_Euler/Problem_18.py
+++ Project_Euler/Problem_18.py
@@ -1,4 +1,3 @@
-
 tree = """75
 95 64
 17 47 82
@@ -16,76 +15,33 @@ tree = """75
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 
 
-# Python program for Kruskal's algorithm to find
-# Minimum Spanning Tree of a given connected, 
-# undirected and weighted graph
-import sys  # Library for INT_MAX
- 
+
 class Graph:
  
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)] 
-                      for row in range(vertices)]
- 
-    
+    def __init__(self,vertices):
+        self.V= vertices 
+        self.graph = []
+  
     def addedge(self,u,v,w):
-        self.graph[u][v] = w
+        if([u,v,w] not in self.graph):
+            self.graph.append([u, v, w])
+         
 
-    # A utility function to print the constructed MST stored in parent[]
-    def printMST(self, parent):
-        print "Edge \tWeight"
-        for i in range(1,self.V):
-            print parent[i],"-",i,"\t",self.graph[i][ parent[i] ]
+    def solve(self, src): # Varient of BellMan Ford Algo
+
+        dist = [0] * self.V
+        dist[src] = 0
  
-    # A utility function to find the vertex with minimum distance value, from
-    # the set of vertices not yet included in shortest path tree
-    def minKey(self, key, mstSet):
- 
-        # Initilaize min value
-        min = sys.maxint
- 
-        for v in range(self.V):
-            if key[v] < min and mstSet[v] == False:
-                min = key[v]
-                min_index = v
- 
-        return min_index
- 
-    # Function to construct and print MST for a graph represented using
-    # adjacency matrix representation
-    def primMST(self):
- 
-        #Key values used to pick minimum weight edge in cut
-        key = [sys.maxint] * self.V
-        parent = [None] * self.V # Array to store constructed MST
-        key[0] = 0   # Make key 0 so that this vertex is picked as first vertex
-        mstSet = [False] * self.V
- 
-        parent[0] = -1  # First node is always the root of
- 
-        for cout in range(self.V):
- 
-            # Pick the minimum distance vertex from the set of vertices not
-            # yet processed. u is always equal to src in first iteration
-            u = self.minKey(key, mstSet)
- 
-            # Put the minimum distance vertex in the shortest path tree
-            mstSet[u] = True
- 
-            # Update dist value of the adjacent vertices of the picked vertex
-            # only if the current distance is greater than new distance and
-            # the vertex in not in the shotest path tree
-            for v in range(self.V):
-                # graph[u][v] is non zero only for adjacent vertices of m
-                # mstSet[v] is false for vertices not yet included in MST
-                # Update the key only if graph[u][v] is smaller than key[v]
-                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-                        key[v] = self.graph[u][v]
-                        parent[v] = u
- 
-        self.printMST(parent)
-# Driver code
+        for i in range(self.V - 1):
+            change = True
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+                        change = False
+
+            if(change):break
+                
+        print min(dist) * -1
 
 mapover = {0:0}
 def mapper(val):
@@ -95,27 +51,20 @@ def mapper(val):
 
 cleantree = [map(int , i.split()) for i in tree.split("\n")]
 
+cleantree[1][0] += cleantree[0][0]
+cleantree[1][1] += cleantree[0][0] 
+
 newtree = [ [ mapper(i) for i in j  ] for j in cleantree ]
 
-
 g = Graph(max(mapover.keys()) + 1)
-
+root = g
 stack = [(0,0)]
 
-while True:
-    if(stack == []):break
-    current = newtree[stack[0][0]][stack[0][1]]
-    newv = newtree[stack[0][0] + 1][stack[0][1]]
-    g.addedge(current , newv , mapover[newv]  )
-    newv = newtree[stack[0][0] + 1][stack[0][1] + 1]
-    g.addedge(current , newv , mapover[newv]  )
-    if(stack[0][0]  < (len(newtree) - 2)):
-        stack.append( (stack[0][0] + 1 , stack[0][1] ) )
-        stack.append( (stack[0][0] + 1 , stack[0][1] + 1 ) )
-    del stack[0]
+for i in range(len(newtree) -1):
+    for m in range(i + 1):
+        g.addedge(newtree[i][m] , newtree[i+1][m] , -1 *mapover[newtree[i+1][m]])
+        g.addedge(newtree[i][m] , newtree[i+1][m+1] , -1 *mapover[newtree[i+1][m+1]])
 
-print g.graph
-
-g.primMST()
+g.solve(1)
     
  
