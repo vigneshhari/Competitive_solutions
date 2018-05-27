c740a7ab97354553b9e25535b1ee12fcf6e8883f
Vignesh Hari <vichuhari100@gmail.com>
2018-05-27 19:05:11 +0530

    Partially Solved Another One

--- watson_array_love_codesprint.py
+++ watson_array_love_codesprint.py
@@ -7,8 +7,6 @@ for _ in range(input()):
     temp_prod = 1
     crazy = 0
     for i in array:
-        i = i * temp_prod
-        if(i < m and i != 0 and (k == i % m) ):temp_prod = i ;crazy = 1;continue
         temp = i - m * (i//m)
         temp = temp - k
         if(temp == 0):
@@ -18,7 +16,6 @@ for _ in range(input()):
             counter += crazy
             crazy = 0
         else:
-            temp_prod = i
             temper+=1
         pos+=1
         #print temp,
