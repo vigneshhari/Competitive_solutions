60fe67f36e148b5c4fc83b439e46d53fb496cb19
Vignesh Hari <vichuhari100@gmail.com>
2018-06-01 00:21:57 +0530

    Palindrome Index Solved

---
+++ palindrome-index.py
@@ -0,0 +1,31 @@
+def palindrome_check(string):
+    i = 0 
+    j = len(string) - 1
+    while i <= j:
+        if( string[i] != string[j]):return False
+        i+=1
+        j-=1
+    return True
+
+
+for _ in range(input()):
+    inp = raw_input()
+    i = 0 
+    j = len(inp) -1
+    done = True
+    while i <= j:
+        if(inp[i] != inp[j]):
+            print inp[0:i] + inp[i+1:]
+            print inp[0:j] + inp[j+1 : ]
+            if palindrome_check(inp[0:i] + inp[i+1:]):
+                done = False
+                print i
+                break
+            elif palindrome_check(inp[0:j] + inp[j+1 : ]):
+                done = False
+                print j
+                break
+        i+=1
+        j-=1
+    if(done):
+        print "-1"
