75c5c0e2d69f6373a112e8379af73899d89854e9
Vignesh Hari <vichuhari100@gmail.com>
2018-06-07 16:42:51 +0530

    Added New Data structure

---
+++ DAA/Python/LinkedList.py
@@ -0,0 +1,27 @@
+import math
+import os
+import random
+import re
+import sys
+from __future__ import print_function
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
