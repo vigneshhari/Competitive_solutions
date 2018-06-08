4825d3c46f732d009c2face5415e3f43deacbfdf
Vignesh Hari <vichuhari100@gmail.com>
2018-06-09 00:33:44 +0530

    Disjoint Set ADT

--- mergingcommunities.py
+++ mergingcommunities.py
@@ -1,26 +1,33 @@
 class Size_Set:
 
     def __init__(self,size):
-        self.Makeset(size)
-    
-    def Makeset(self,size):
-        self.S = [-1 ] * (size+2)
-    
-    def Find(self,x):
-        if(self.S[x] < 0):
-            return x
-        else:
-            return self.Find(self.S[x])
-    
+        self.ds = [ [i] for i in xrange(size+1) ]
 
-    def Union(self,root1,root2):
-        if(self.Find(root1) == self.Find(root2) and self.Find(root1) == -1):return
-        if(self.S[root2] < self.S[root1]):
-            self.S[root2] += self.S[root1]
-            self.S[root1] = root2 
+    def Union(self,e1 , e2):
+        e1 = self.Find(e1)
+        e2 = self.Find(e2)
+        if(e1 == e2):return 
+        l1  = self.ds[e1]
+        l2 = self.ds[e2]
+        if(len(l1) > len(l2)):
+            self.ds[e1] = self.ds[e1] +  self.ds[e2]
+            for i in self.ds[e2]:
+                self.ds[i] = e1
         else:
-            self.S[root1] += self.S[root2]
-            self.S[root2] = root1
+            self.ds[e2] = self.ds[e2] +  self.ds[e1]
+            for i in self.ds[e1]:
+                self.ds[i] = e2
+
+
+    def Find(self , el):
+        if(type(self.ds[el]) == int):
+            return self.ds[el]
+        return el
+
+    def Finder(self,el):
+        if(type(self.ds[el]) == int):
+            return self.ds[self.ds[el]]
+        return self.ds[el]
 
 
 n ,q = map(int , raw_input().split())
@@ -28,6 +35,17 @@ comm = Size_Set(n)
 for _ in xrange(q):
     inp = raw_input().split()
     if(inp[0] == "Q"):
-        print -1 * comm.S[comm.Find(int(inp[1]))]
+        print len(comm.Finder(int(inp[1])))
     else:
         comm.Union(int(inp[1]) , int(inp[2]))
+
+'''
+10 10
+M 1 2
+M 1 3
+M 10 9
+M 10 1
+M 5 7
+M 10 2
+Q 2
+'''
