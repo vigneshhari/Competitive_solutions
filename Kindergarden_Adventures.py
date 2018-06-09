470c7efa0f448a7827e4397591c5438a8ec5c2f9
Vignesh Hari <vichuhari100@gmail.com>
2018-06-10 02:09:51 +0530

    Solved Kindergarden Adventures

--- Kindergarden_Adventures.py
+++ Kindergarden_Adventures.py
@@ -4,7 +4,6 @@ arr = [0] * (n)
 for j,i in enumerate(lis):
     if(i >= n ):continue
     if(i == 0):continue
-    #print arr , j +1 , j - i + 1
     arr[(j+1) % n] += 1
     arr[j-i + 1 ] -= 1
 maxv = -999999999999999
@@ -16,7 +15,3 @@ for j,i in enumerate(arr):
         maxv = temp
         maxp = j
 print maxp + 1
-'''
-6
-1 5 4 3 6 8
-'''
