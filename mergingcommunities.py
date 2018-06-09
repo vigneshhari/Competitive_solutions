086471f2455372e37e61a35d177534ecdf93e6fb
Vignesh Hari <vichuhari100@gmail.com>
2018-06-09 21:34:41 +0530

    Solved Mergin Communities Entirely

--- mergingcommunities.py
+++ mergingcommunities.py
@@ -1,29 +1,33 @@
 class Size_Set:
-    def __init__(self, n):
-        self._id = list(range(n))
-        self._sz = [1] * n
 
-    def _root(self, i):
-        j = i
-        while (j != self._id[j]):
-            self._id[j] = self._id[self._id[j]]
-            j = self._id[j]
-        return j
+    def __init__(self,size):
+        self.ds = [ [i] for i in xrange(size+1) ]
 
-    def Find(self, p, q):
-        return self._root(p) == self._root(q)
-    
-    def Union(self, p, q):
-        i = self._root(p)
-        j = self._root(q)
-        if i == j:
-            return
-        if (self._sz[i] < self._sz[j]):
-            self._id[i] = j
-            self._sz[j] += self._sz[i]
+    def Union(self,e1 , e2):
+        e1 = self.Find(e1)
+        e2 = self.Find(e2)
+        if(e1 == e2):return 
+        l1  = self.ds[e1]
+        l2 = self.ds[e2]
+        if(len(l1) > len(l2)):
+            for i in self.ds[e2]:
+                self.ds[e1] += [i] 
+                self.ds[i] = e1
         else:
-            self._id[j] = i
-            self._sz[i] += self._sz[j]
+            for i in self.ds[e1]:
+                self.ds[e2] += [i] 
+                self.ds[i] = e2
+
+
+    def Find(self , el):
+        if(type(self.ds[el]) == int):
+            return self.ds[el]
+        return el
+
+    def Finder(self,el):
+        if(type(self.ds[el]) == int):
+            return self.ds[self.ds[el]]
+        return self.ds[el]
 
 
 n ,q = map(int , raw_input().split())
@@ -31,17 +35,6 @@ comm = Size_Set(n)
 for _ in xrange(q):
     inp = raw_input().split()
     if(inp[0] == "Q"):
-        print  comm._sz[comm._id[int(inp[1])]]
+        print len(comm.Finder(int(inp[1])))
     else:
         comm.Union(int(inp[1]) , int(inp[2]))
-
-'''
-10 10
-M 1 2
-M 1 3
-M 10 9
-M 10 1
-M 5 7
-M 10 2
-Q 2
-'''
