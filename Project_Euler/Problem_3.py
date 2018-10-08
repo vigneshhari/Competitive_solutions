51528ab0a211e53f75c96be9fc7b885f5ff26fa3
Mathew <matthewzalex@gmail.com>
2018-10-08 11:00:02 +0530

    Modified for optimization
    check only primes before root

--- Project_Euler/Problem_3.py
+++ Project_Euler/Problem_3.py
@@ -1,18 +1,11 @@
-val = 600851475143
-i = int(val ** .5)
-
-
-def normal_prime(num):
-    if(num == 1):return 0
-    if(num < 3 ):return 1
-    if(num % 2 == 0 ):return 0 
-    for i in xrange(3,int(num ** 0.5)+1,2):
-        if(num % i == 0):return 0
-    return 1
-
-while (i > 0):
-    
-    if(val % i == 0):
-        if(normal_prime(i) == 1):
-            print i
-    i -= 1 
+n = 600851475143
+i=2
+while i*i < n:
+    if n % i == 0:
+        while n%i == 0:
+            n//=i
+    i+=1
+if n%i == 0:
+    print(i)
+else:
+    print(n)
