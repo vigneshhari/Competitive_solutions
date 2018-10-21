92ce2d3a891e25b2d6f9ec8f738946bc57e4345e
Vignesh Hari <vichuhari100@gmail.com>
2018-10-21 18:47:36 +0530

    Basic Rubble for Question Magic in IEEE Xtreme 12

---
+++ xtreme/magic.py
@@ -0,0 +1,25 @@
+"""
+
+
+121 101 112 32 109 111 109 114 10 109 111 109 114 32 114 111 104 106 121 32 100 114 98 114 109
+"""
+yep -> two
+pmr -> one
+momr -> nine
+gobr -> five
+rohjy -> eight
+drbrm -> seven
+xrtp -> zero
+
+a b c d e f g h i j k l m n o p q r s t u v w x y z
+- v - s w - f g - h - - n - i o - e - r - - - z t -
+
+
+
+2 9
+9 8 7
+
+
+2 1
+1 9 7
+2 0 5 9
