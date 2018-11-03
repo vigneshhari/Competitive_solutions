28a3794c205c6266942c143c1268d8ca8b9b6164
Vignesh Hari <vichuhari100@gmail.com>
2018-11-03 14:42:34 +0300

    Number of Required Rotations to unlock circular lock

---
+++ geeksforgeeks/min_rotations.py
@@ -0,0 +1,22 @@
+
+#Python2
+
+'''
+
+2
+222 333
+2345 5432
+
+'''
+
+
+for _ in range(int(input())):
+    startl , endl = input().split()
+    startl = list(map(int , list(startl)))
+    endl = list(map(int, list(endl)))
+    ans = 0
+    for i in range(len(startl)):
+        startpos = startl[i]
+        endpos = endl[i]
+        ans += min( abs(endpos - startpos) , ((endpos + 10) - startpos)   % 10  , ((startpos + 10) - endpos)   % 10 )
+    print(ans)
