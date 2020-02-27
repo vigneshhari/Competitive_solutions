c16725bec483c9f49817df991685d14eb4f56f6d
Vignesh Hari <vichuhari100@gmail.com>
2020-02-27 15:17:12 +0530

    Purple Rain Updated

--- Processing/3_PurpleRain/Rain.js
+++ Processing/3_PurpleRain/Rain.js
@@ -4,16 +4,15 @@ Title : Coding train Challenge 3 : Purple Rain
 Date : 27th February 2020
 */
 
-
-var rain_drops = []
-
-const num_rain_drops = 500
 const width = screen.width
 const height = screen.height - (7.31 / 100 * screen.height)
-var speed = 10
+const num_rain_drops = 500
 const min_speed = 5
+const speed = 10
 const drop_len = 50
 const max_thickness = 8
+
+var rain_drops = []
 
 function setup() {
     createCanvas(width, height);
@@ -23,16 +22,12 @@ function setup() {
 }
 
 function draw() {
-    speed = map(mouseX, 0, width, 5, 50);
-    //translate(width / 2, height / 2);
     background(230, 230, 250);
     for (var i = 0; i < rain_drops.length; i++) {
-
         rain_drops[i].update();
         rain_drops[i].show();
     }
 }
-
 
 
 class Drop {
@@ -41,13 +36,11 @@ class Drop {
 
     constructor() {
         this.x = random(0, width)
-        this.y = random(0, -100)
+        this.y = random(-100, -200)
         this.z = random(0, this.max_z)
         this.drop_speed = map(this.z, 0, this.max_z, speed - 10, speed)
         this.drop_len = map(this.z, 0, this.max_z, 1, drop_len)
         this.drop_thickness = map(this.z, 0, this.max_z, 1, max_thickness)
-        //this.last_z = this.z
-
     }
 
     update() {
@@ -58,9 +51,7 @@ class Drop {
         }
     }
 
-
     show() {
-
         fill(255)
         stroke(138, 43, 226);
         strokeWeight(this.drop_thickness)
