fab8626310c015dbf2abaffe76b4ddc669c315ae
Vignesh Hari <vichuhari100@gmail.com>
2020-02-27 22:22:59 +0530

    Started Working on Snakes

---
+++ Processing/5_Snake/Snake.js
@@ -0,0 +1,20 @@
+/*
+Author : Vignesh Hari
+Title : Coding train Challenge 5 : Snake
+Date : 27th February 2020
+*/
+
+const width = 500
+const height = 500
+
+function setup() {
+    createCanvas(width, height);
+    background(51);
+
+}
+
+function draw() {
+    fill(255);
+    rect(479, 0, 20, 20);
+
+}
