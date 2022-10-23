705bc295fc56e6302f259d4cd60a19b8baf75fea
Vignesh Hari <vichuhari100@gmail.com>
2022-10-23 21:13:18 +0530

    Solved Problems

---
+++ leetcode/golang/completed_all/203-remove-linked-list-elements/203-remove-linked-list-elements.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addAtTail(this *ListNode, val int) *ListNode {
+	lastNode := this
+	if lastNode != nil {
+		for lastNode.Next != nil {
+			lastNode = lastNode.Next
+		}
+	}
+	newObj := ListNode{}
+	newObj.Val = val
+	newObj.Next = nil
+	if lastNode == nil {
+		return &newObj
+	} else {
+		lastNode.Next = &newObj
+	}
+	return this
+
+}
+
+func (this *ListNode) Print() {
+	currentNode := this
+	for true {
+		fmt.Print(currentNode.Val, " | ")
+		currentNode = currentNode.Next
+		if currentNode == nil {
+			break
+		}
+	}
+	fmt.Println()
+}
+
+func createLinkedList(list []int) *ListNode {
+	var newListNode *ListNode = nil
+	for _, j := range list {
+		newListNode = addAtTail(newListNode, j)
+	}
+	return newListNode
+}
+
+func createNode(Val int) *ListNode {
+	newObj := ListNode{}
+	newObj.Val = Val
+	newObj.Next = nil
+	return &newObj
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	fakeHead := createNode(-101)
+	fakeHead.Next = head
+	looper := head
+	previouslooper := fakeHead
+	for looper != nil {
+		if looper.Val == val {
+			previouslooper.Next = looper.Next
+		} else {
+			previouslooper = previouslooper.Next
+		}
+		looper = looper.Next
+	}
+	return fakeHead.Next
+}
+
+func main() {
+	newList1 := createLinkedList([]int{5, 5, 5, 5, 5, 5})
+	result := removeElements(newList1, 5)
+	fmt.Print("Result is : ")
+	if result != nil {
+		result.Print()
+	}
+}
